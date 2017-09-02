7646cc2c11c7162391e6989aff38cca0ec065415
Soriyath Straessle <soriyath@gmail.com>
2017-09-02 14:42:52 +0200

    fixed typos

--- bmark/evolution_bmark.ex
+++ bmark/evolution_bmark.ex
@@ -11,19 +11,19 @@ defmodule GeneticAlgorithm do
     Evolution.guess "My name is Alice.", 500, 0.05
   end
 
-  bmark :pop_1_000_rate_1pc, runs: 5 do, 1_000, 0.01
-    Evolution.guess "My name is Alice."
+  bmark :pop_1_000_rate_1pc, runs: 5 do
+    Evolution.guess "My name is Alice.", 1_000, 0.01
   end
 
-  bmark :pop_1_000_rate_5pc, runs: 5 do, 1_000, 0.05
-    Evolution.guess "My name is Alice."
+  bmark :pop_1_000_rate_5pc, runs: 5 do
+    Evolution.guess "My name is Alice.", 1_000, 0.05
   end
 
-  bmark :pop_5_000_rate_1pc, runs: 5 do, 5_000, 0.01
-    Evolution.guess "My name is Alice."
+  bmark :pop_5_000_rate_1pc, runs: 5 do
+    Evolution.guess "My name is Alice.", 5_000, 0.01
   end
 
-  bmark :pop_5_000_rate_5pc, runs: 5 do, 5_000, 0.05
-    Evolution.guess "My name is Alice."
+  bmark :pop_5_000_rate_5pc, runs: 5 do
+    Evolution.guess "My name is Alice.", 5_000, 0.05
   end
 end
